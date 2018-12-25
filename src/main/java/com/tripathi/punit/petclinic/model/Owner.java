e59f5c35fc634f619720156bf9b6e0bf3bd488af
trinup <ptripathi50@gmail.com>
2018-12-25 21:05:33 +0530

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/tripathi/punit/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.tripathi.punit.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/tripathi/punit/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.tripathi.punit.petclinic.model;
+
+public class Vet extends Person {
+}
